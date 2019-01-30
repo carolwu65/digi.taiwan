b55936174592ac7f9dbc560193b9b932c07200c2
mark19891107 <mark19891107@gmail.com>
2019-01-30 20:57:32 +0800

    update digimap

--- digimap/js/common.js
+++ digimap/js/common.js
@@ -2,47 +2,96 @@ $(document).ready(function(){
 	
 	$('img[usemap]').rwdImageMaps();
 	$('area').rwdImageColor();
-	
+	//$('area').createInitImg();
+
 	$(window).bind("resize", function(){
 		$('.changeimg').hide();
+		//$('area').createInitImg();
 	});
 });
 
 ;(function($) {
+
+	$.fn.createInitImg = function(){
+		var ass = $(this);
+		
+		ass.each(function(){
+			var area = $(this);
+			
+			var pos = area.attr('coords').split(',');
+			var pimg = $("#" + area.data('pimg'));
+			var puse = area.data('use');
+			var img = $("#imgn" + area.attr('id'));
+			
+			if($(window).width() >= 680){
+				if(puse=='mobile') return;
+			}else{
+				if(puse=='pc') return;
+			}
+			
+			if(area.data('img')!=''){
+				if(!img.length){
+					$('body').append("<a href='"+area.attr('href')+"' target='_blank'><img id='imgn" + area.attr('id') + "' src='/digimap/images/" + area.data('img') + "-01.png' class='initimg'></a>");
+					img = $("#imgn" + area.attr('id'));
+				}else{
+					img.attr('src', '/digimap/images/' + area.data('img') + '-01.png');
+				}
+			}
+			
+			var ppos = pimg.position();
+			var pw = pimg.width() / 1280;
+			
+			x = +pos[0] + ppos.left - (area.data("x") * pw);
+			y = +pos[1] + ppos.top + (area.data("y") * pw);
+			
+			img.css("position","absolute");
+			img.css("z-index","99");
+			img.css("top", y + "px");
+			img.css("left", x + "px");
+			img.css("width", (area.data("cw") * pw) + "px");
+			img.show();
+		});
+	};
+
 	$.fn.rwdImageColor = function() {
 		var ass = $(this);
 		
 		ass.each(function(){
 			var area = $(this);
+			
 			area.mouseover(function(e){
 				var th = $(this);
 				var pos = th.attr('coords').split(',');
 				var pimg = $("#" + th.data('pimg'));
 				var img = $("#img" + th.attr('id'));
 
-				if(!img.length){
-					$('body').append("<a href='"+th.attr('href')+"' target='_blank'><img id='img" + th.attr('id') + "' src='/digimap/images/" + th.data('img') + ".png' class='changeimg'></a>");
-					img = $("#img" + th.attr('id'));
+				if(th.data('img')!=''){
+					if(!img.length){
+						$('body').append("<a href='"+th.attr('href')+"' target='_blank'><img id='img" + th.attr('id') + "' src='/digimap/images/" + th.data('img') + "-02.png' class='changeimg'></a>");
+						img = $("#img" + th.attr('id'));
+					}else{
+						img.attr('src', '/digimap/images/' + th.data('img') + '-02.png');
+					}
+
+					var ppos = pimg.position();
+					var pw = pimg.width() / 1280;
+					
+					x = +pos[0] + ppos.left - (th.data("x") * pw);
+					y = +pos[1] + ppos.top + (th.data("y") * pw);
+
+					img.css("position","absolute");
+					img.css("z-index","99");
+					img.css("top", y + "px");
+					img.css("left", x + "px");
+					img.css("width", (th.data("cw") * pw) + "px");
+					img.show();
+					
+					img.mouseover(function(e){
+						$('.changeimg').hide();
+						var ig = $(this);
+						ig.show();
+					});
 				}
-
-				var ppos = pimg.position();
-				var pw = pimg.width() / 1280;
-				
-				x = +pos[0] + ppos.left - (th.data("x") * pw);
-				y = +pos[1] + ppos.top + (th.data("y") * pw);
-
-				img.css("position","absolute");
-				img.css("z-index","99");
-				img.css("top", y + "px");
-				img.css("left", x + "px");
-				img.css("width", (th.data("cw") * pw) + "px");
-				img.show();
-				
-				img.mouseover(function(e){
-					$('.changeimg').hide();
-					var ig = $(this);
-					ig.show();
-				});
 			});
 			area.mouseout(function(e){
 				var th = $(this);
